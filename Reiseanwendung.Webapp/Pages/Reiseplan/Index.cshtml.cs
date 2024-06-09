515da1d7dc71580e668faa8ae2a8a99c32d4f325
Edren Arnaez <arn210272@spengergasse.at>
2024-06-09 20:13:46 +0200

    anzahl objekte

--- Reiseanwendung.Webapp/Pages/Reiseplan/Index.cshtml.cs
+++ Reiseanwendung.Webapp/Pages/Reiseplan/Index.cshtml.cs
@@ -1,35 +1,33 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Reiseanwendung.Application.Infrastructure;
 using Reiseanwendung.Application.Model;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Reiseanwendung.Webapp.Pages.Reiseplan
 {
     public class IndexModel : PageModel
     {
         private readonly ReiseplanContext _db;
-        public List<TravelPlan> Travelplans { get; set; } = new();
 
         public IndexModel(ReiseplanContext db)
         {
             _db = db;
         }
 
+        public List<TravelPlan> Travelplans { get; set; }
+
         public void OnGet()
         {
-
-        
-                  
-                Travelplans = _db.TravelPlans
-
-                    .Include(tp => tp.Destinations)                
-                    .OrderByDescending(t => t.StartDate)
-                    .ToList();
-            
-
-
+            Travelplans = _db.TravelPlans
+                .Include(tp => tp.Destinations)
+                    .ThenInclude(d => d.Activities)
+                .Include(tp => tp.Destinations)
+                    .ThenInclude(d => d.Accommodations)
+                .Include(tp => tp.Destinations)
+                    .ThenInclude(d => d.Transportations)
+                .ToList();
         }
     }
 }
